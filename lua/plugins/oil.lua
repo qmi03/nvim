8fca8efec537597b77d47a7ecd4915fa013cfa4d
qmi <minh.pham1126@gmail.com>
2025-04-26 03:50:33 +0700

    unclutter some oil settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -14,10 +14,15 @@ return {
       -- Id is automatically added at the beginning, and name at the end
       -- See :help oil-columns
       columns = {
-        "icon",
-        -- "permissions",
-        -- "size",
-        -- "mtime",
+        { "icon",       add_padding = true }, -- Icon for the entry (requires nvim-web-devicons)
+        { "permissions" },                    -- Access permissions of the file
+        -- Other available columns (commented out):
+        -- { "type" },                 -- Type of the entry (file, directory, link, etc)
+        -- { "size" },                 -- Size of the file
+        -- { "ctime" },                -- Change timestamp of the file
+        -- { "mtime" },                -- Last modified time of the file
+        -- { "atime" },                -- Last access time of the file
+        -- { "birthtime" },            -- Time the file was created
       },
       -- Buffer-local options to use for oil buffers
       buf_options = {
@@ -36,9 +41,9 @@ return {
         concealcursor = "nvic",
       },
       -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-      delete_to_trash = false,
+      delete_to_trash = true,
       -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
-      skip_confirm_for_simple_edits = false,
+      skip_confirm_for_simple_edits = true,
       -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
       -- (:help prompt_save_on_select_new_entry)
       prompt_save_on_select_new_entry = true,
@@ -57,9 +62,9 @@ return {
       },
       -- Constrain the cursor to the editable parts of the oil buffer
       -- Set to `false` to disable, or "name" to keep it on the file names
-      constrain_cursor = "editable",
+      constrain_cursor = false,
       -- Set to true to watch the filesystem for changes and reload oil
-      watch_for_changes = false,
+      watch_for_changes = true,
       -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
       -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
       -- Additionally, if it is a string that matches "actions.<name>",
@@ -69,21 +74,6 @@ return {
       keymaps = {
         ["g?"] = "actions.show_help",
         ["<CR>"] = "actions.select",
-        ["<C-s>"] = {
-          "actions.select",
-          opts = { vertical = true },
-          desc = "Open the entry in a vertical split",
-        },
-        ["<C-h>"] = {
-          "actions.select",
-          opts = { horizontal = true },
-          desc = "Open the entry in a horizontal split",
-        },
-        ["<C-t>"] = {
-          "actions.select",
-          opts = { tab = true },
-          desc = "Open the entry in new tab",
-        },
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
         ["<C-l>"] = "actions.refresh",
@@ -100,9 +90,25 @@ return {
         ["gx"] = "actions.open_external",
         ["g."] = "actions.toggle_hidden",
         ["g\\"] = "actions.toggle_trash",
+        -- Additional useful keymaps:
+        ["gy"] = "actions.yank_entry", -- Yank the filepath to register
+        ["<C-t>"] = { -- Open in a new tab
+          "actions.select",
+          opts = { tab = true }
+        },
+        ["<leader>t"] = "actions.open_terminal", -- Open terminal in current directory
+        ["<leader>q"] = "actions.send_to_qflist", -- Send files to quickfix list
+        ["<C-f>"] = {                       -- Preview scroll down
+          "actions.preview_scroll_down",
+          mode = "n",
+        },
+        ["<C-b>"] = { -- Preview scroll up
+          "actions.preview_scroll_up",
+          mode = "n",
+        },
       },
       -- Set to false to disable all of the above keymaps
-      use_default_keymaps = true,
+      use_default_keymaps = false,
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
@@ -112,7 +118,13 @@ return {
         end,
         -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
-          local always_hidden = { "__pycache__", ".DS_Store" }
+          local always_hidden = {
+            "__pycache__",
+            ".DS_Store",
+            "node_modules",
+            ".git",
+            ".vscode"
+          }
           for _, hidden_name in ipairs(always_hidden) do
             if name == hidden_name then
               return true
@@ -122,15 +134,14 @@ return {
         end,
         -- Sort file names in a more intuitive order for humans. Is less performant,
         -- so you may want to set to false if you work with large directories.
-        natural_order = true,
+        natural_order = "fast",
         -- Sort file and directory names case insensitive
         case_insensitive = false,
         sort = {
-          -- sort order can be "asc" or "desc"
-          -- see :help oil-columns to see which columns are sortable
-          { "type", "asc" },
-          { "name", "asc" },
-        },
+          { "type",  "asc" },
+          { "name",  "asc" },
+          { "mtime", "desc" }, -- Show newest files first
+        }
       },
       -- Extra arguments to pass to SCP when moving/copying files over SSH
       extra_scp_args = {},
