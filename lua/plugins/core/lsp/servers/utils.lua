1e0f26ef39ec42115a8d3b411ad21ab681acfd2c
qmi <minh.pham1126@gmail.com>
2025-08-30 07:16:25 +0700

    navic: attach navic to lsp

--- lua/plugins/core/lsp/servers/utils.lua
+++ lua/plugins/core/lsp/servers/utils.lua
@@ -6,22 +6,15 @@ return {
     end
     local opts = { noremap = true, silent = true }
     buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gD", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-    buf_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap(
-      "n",
-      "<leader>ll",
-      "<cmd>lua vim.lsp.codelens.run()<cr>",
-      opts
-    )
     buf_set_keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    vim.diagnostic.config { update_in_insert = true }
+    buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+
+
     client.server_capabilities.document_formatting = true
+
+    local navic = require("nvim-navic")
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
   end,
 }
