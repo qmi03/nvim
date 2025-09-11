b304f87f62fa03127b16434f1090dd4afc05833c
qmi <minh.pham1126@gmail.com>
2025-09-12 02:55:47 +0700

    utils: change silicon plugins (still doesn't work)

--- lua/plugins/tool/media.lua
+++ lua/plugins/tool/media.lua
@@ -1,13 +1,38 @@
 return {
-  "AndrewRadev/id3.vim",
+  { "AndrewRadev/id3.vim" },
   {
-    "michaelrommel/nvim-silicon",
-    lazy = true,
-    cmd = "Silicon",
+    "narutoxy/silicon.lua",
+    requires = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("silicon").setup {
-        font = "JetBrainsMonoNL Nerd Font=34;",
-      }
-    end,
+      local silicon = require 'silicon'
+      silicon.setup({
+        theme = "auto",
+        -- auto generate file name based on time (absolute or relative to cwd)
+        output = "SILICON_${year}-${month}-${date}_${time}.png",
+        bgColor = vim.g.terminal_color_5,
+        roundCorner = true,
+        windowControls = true,
+        lineNumber = true,
+        font = "monospace",
+        lineOffset = 1, -- from where to start line number
+        linePad = 2,    -- padding between lines
+        padHoriz = 80,  -- Horizontal padding
+        padVert = 100,  -- vertical padding
+        shadowBlurRadius = 10,
+        shadowColor = "#555555",
+        shadowOffsetX = 8,
+        shadowOffsetY = 8,
+        gobble = false, -- enable lsautogobble like feature
+        debug = false,  -- enable debug output
+      })
+      -- Generate image of lines in a visual selection
+      vim.keymap.set('v', '<Leader>s', function() silicon.visualise_api({}) end)
+      -- Generate image of a whole buffer, with lines in a visual selection highlighted
+      vim.keymap.set('v', '<Leader>bs', function() silicon.visualise_api({ to_clip = true, show_buf = true }) end)
+      -- Generate visible portion of a buffer
+      vim.keymap.set('n', '<Leader>s', function() silicon.visualise_api({ to_clip = true, visible = true }) end)
+      -- Generate current buffer line in normal mode
+      vim.keymap.set('n', '<Leader>s', function() silicon.visualise_api({ to_clip = true }) end)
+    end
   },
 }
