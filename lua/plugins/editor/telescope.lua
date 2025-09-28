3a5e1c227191d44a465441087379901473deb89c
qmi <minh.pham1126@gmail.com>
2025-09-28 07:48:59 +0700

    config(keymap): add some keymaps for lsp and snippet engine

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -65,6 +65,12 @@ return {
           telescope.extensions.zoxide.list,
           desc = "Zoxide directory list",
         },
+        ["fs"] = {
+          function()
+            builtin.lsp_workspace_symbols()
+          end,
+          desc = "Get workspace symbols",
+        }
       }
 
       for key, mapping in pairs(keymaps) do
@@ -106,9 +112,9 @@ return {
                 after_action = function(selection)
                   print(
                     "Changed to: ("
-                      .. selection.z_score
-                      .. ") "
-                      .. selection.path
+                    .. selection.z_score
+                    .. ") "
+                    .. selection.path
                   )
                 end,
               },
